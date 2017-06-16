94b6bce99893c99b63462cfb7f0f2bfbb0f308e5
ShatteredIce <icywind.katsuo@gmail.com>
2017-06-16 12:14:33 -0700

    initial commit

---
+++ app/src/main/java/com/example/npurwosumarto/sparkleanimation/MyGLRenderer.java
@@ -0,0 +1,85 @@
+package com.example.npurwosumarto.sparkleanimation;
+
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.SystemClock;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by 301968 on 6/15/2017.
+ */
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private Triangle mTriangle;
+
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        mTriangle = new Triangle();
+    }
+
+    //private float[] mTranslationMatrix = new float[16];
+    int counter = 0;
+    float x_offset = 0;
+    float y_offset = 0;
+    float y_movement = 0.05f;
+
+    public void onDrawFrame(GL10 unused) {
+        float[] scratch = new float[16];
+        // Redraw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        Matrix.translateM(mMVPMatrix, 0, x_offset, y_offset, 0);
+
+        counter++;
+        if(counter % 5 == 0) {
+            y_offset += y_movement;
+        }
+
+        if(counter == 50){
+            y_movement *= -1;
+            counter = 0;
+        }
+
+        // Draw triangle
+        mTriangle.draw(mMVPMatrix);
+    }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+}
